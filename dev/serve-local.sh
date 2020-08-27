d8264b4e7431827dfa6b38771d9292f128f49cfa
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-08-27 16:32:32 +0200

    switched LICENSE to BSD3; ran 'elm-test init'

---
+++ dev/serve-local.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+cd ..
+
+elm reactor
---
+++ src/Gammoids/Combinatorics/OrderedNchooseM.elm
@@ -0,0 +1,6 @@
+module Gammoids.Combinatorics.OrderedNchooseM exposing ()
+
+{-| This module provides iterables for ordered subset generation of a fixed size in reverse-lexicographic order with respect to an order given as a list of unique elements.
+
+
+-}
